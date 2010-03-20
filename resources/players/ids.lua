c0581e44c71393951249c1da36249739979ab007
Jumba <jumba990@gmail.com>
2010-03-20 02:19:55 -0400

    Fixed a broken if..else statement
    Implemented search by partial name.

--- resources/players/ids.lua
+++ resources/players/ids.lua
@@ -39,16 +39,19 @@ function getFromName( player, targetName )
 		local match = { }
 		if targetName == "*" then
 			match = { player }
-		if tonumber( targetName ) then
+		elseif tonumber( targetName ) then
 			match = { ids[ tonumber( targetName ) ] }
-		elseif getPlayerFromName( targetName ) then
-			match = { getPlayerFromName( targetName ) }
+		elseif ( getPlayerFromName ( targetName ) ) then
+			match = { getPlayerFromName ( targetName ) }
+		else	
+			match = getNameMatch ( targetName ) -- returns a table.
 		end
 		
-		-- TODO: Search by partial name
+		-- TODO: Search by partial name || Done - Jumba.
 		
 		if #match == 1 then
 			if isLoggedIn( match[ 1 ] ) then
+				outputChatBox ( getPlayerName ( match[1] ) ) 
 				return match[ 1 ], getPlayerName( match[ 1 ] ):gsub( "_", " " ), getElementData( match[ 1 ], "playerid" )
 			else
 				-- not logged in error
@@ -57,7 +60,27 @@ function getFromName( player, targetName )
 		elseif #match == 0 then
 			return nil -- no player
 		else
-			return nil -- show a list of players
+			outputChatBox ( "Players matching your search are: ", player, 255, 255, 0 )
+			for _, player in ipairs ( match ) do
+				outputChatBox ( getPlayerName ( player ):gsub ( "_", " " ) .. " (" .. getElementData ( player, "playerid" ) .. ")", player, 245, 200, 0 )
+			end	
+			return nil -- more than one player. We list the player names + id.
 		end
 	end
+end
+
+function getNameMatch( targetName )
+local matches = { }
+	if ( targetName ) then
+		for k, player in ipairs ( getElementsByType ( "player" ) ) do
+			local player_n = getPlayerName ( player ):lower()
+			local match = player_n:find( targetName:lower() )
+			if ( match ) then
+				if isLoggedIn ( player ) then
+					matches[ #matches + 1 ] = player
+				end
+			end
+		end
+		return matches	
+	end	
 end
