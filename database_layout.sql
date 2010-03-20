5e3cd841d8f6e69189839a8fd955d5c181da059c
mabako <mabako@gmail.com>
2010-03-20 22:29:45 +0100

    updated the database layout

--- database_layout.sql
+++ database_layout.sql
@@ -23,3 +23,19 @@ CREATE TABLE  `wcf1_user` (
   `activationCode` int(10) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`userID`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+CREATE TABLE  `vehicles` (
+  `vehicleID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `model` int(10) unsigned NOT NULL,
+  `posX` float NOT NULL,
+  `posY` float NOT NULL,
+  `posZ` float NOT NULL,
+  `rotX` float NOT NULL,
+  `rotY` float NOT NULL,
+  `rotZ` float NOT NULL,
+  `numberplate` varchar(8) NOT NULL,
+  `health` int(10) unsigned NOT NULL DEFAULT '1000',
+  `color1` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `color2` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`vehicleID`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
