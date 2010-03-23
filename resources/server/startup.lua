bf67494e2369bd868247862d71ef5ebdb8b3ce5b
mabako <mabako@gmail.com>
2010-03-23 01:13:24 +0100

    game type & few rules for ASE

---
+++ resources/server/startup.lua
@@ -0,0 +1,39 @@
+--[[
+Copyright (c) 2010 MTA: Paradise
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+]]
+
+addEventHandler( "onResourceStart", resourceRoot,
+	function( )
+		setGameType( "MTA: Paradise" )
+		setRuleValue( "author", "mabako, Jumba" )
+		setRuleValue( "homepage", "http://paradisegaming.net" )
+		setRuleValue( "forum", "http://forum.paradisegaming.net" )
+		setRuleValue( "license", "GPL Version 3" )
+		setRuleValue( "source", "http://github.com/marcusbauer/mta-paradise" )
+		setRuleValue( "git-url", "git://github.com/marcusbauer/mta-paradise.git" )
+	end
+)
+
+addEventHandler( "onResourceStop", resourceRoot,
+	function( )
+		removeRuleValue( "author" )
+		removeRuleValue( "homepage" )
+		removeRuleValue( "forum" )
+		removeRuleValue( "license" )
+		removeRuleValue( "source" )
+		removeRuleValue( "git-url" )
+	end
+)
