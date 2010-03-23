9fddb8a214362946681cda0dd36f0dc4a70b22c9
mabako <mabako@gmail.com>
2010-03-23 22:52:34 +0100

    /credits

--- resources/server/credits_c.lua
+++ resources/server/credits_c.lua
@@ -1,4 +1,4 @@
-<!--
+--[[
 Copyright (c) 2010 MTA: Paradise
 
 This program is free software; you can redistribute it and/or modify
@@ -13,8 +13,15 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
--->
-<meta>
-	<script src="credits_c.lua" type="client"/>
-	<script src="startup.lua"/>
-</meta>
+]]
+
+local function showCredits( )
+	outputChatBox( " " )
+	outputChatBox( "MTA:Paradise", 0, 255, 255 )
+	outputChatBox( "  Marcus Bauer <mabako@gmail.com>", 193, 255, 255 )
+	outputChatBox( "  Calvin Wong Loi Sing <jumba990@gmail.com>", 193, 255, 255 )
+	outputChatBox( " " )
+end
+addCommandHandler( "credits", showCredits )
+addCommandHandler( "about", showCredits )
+addCommandHandler( "authors", showCredits )
