b8f4900e8be651db756fc776420f093ba7c40f59
mabako <mabako@gmail.com>
2010-05-01 19:17:56 +0200

    couple of blips

---
+++ resources/interiors/blips.lua
@@ -0,0 +1,6 @@
+local function createBlip2(x, y, z, icon)
+	createBlip( x, y, z, icon, 2, 255, 255, 255, 255, 0, 300 )
+end
+createBlip2(-2766, 375, 6, 41) -- City Hall
+createBlip2(-1605, 710, 14, 30) -- Police Department
+createBlip2(-2659, 605, 14, 22) -- Hospital
