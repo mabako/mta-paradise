38328ec2e9c7c4bea04878b1c24379847612d32f
mabako <mabako@gmail.com>
2010-05-02 09:13:57 +0200

    use sha1 if available

--- resources/players/registration.lua
+++ resources/players/registration.lua
@@ -57,7 +57,7 @@ addEventHandler( getResourceName( resource ) .. ":register", root,
 							end
 							
 							-- create the user
-							if exports.sql:query_free( "INSERT INTO wcf1_user (username,password,salt) VALUES ('%s', SHA1(CONCAT('%s', SHA1(CONCAT('%s', SHA1('%s'))))), '%s')", username, salt, salt, password, salt ) then
+							if exports.sql:query_free( "INSERT INTO wcf1_user (username,salt,password) VALUES ('%s', '%s', SHA1(CONCAT('%s', SHA1(CONCAT('%s', " .. ( sha1 and ( "'" .. sha1( password ) .. "'" ) or "SHA1('%s')" ) .. ")))))", username, salt, salt, salt, not sha1 and password ) then
 								triggerClientEvent( source, getResourceName( resource ) .. ":registrationResult", source, 0 ) -- will automatically login when this is sent
 							else
 								triggerClientEvent( source, getResourceName( resource ) .. ":registrationResult", source, 3 )
