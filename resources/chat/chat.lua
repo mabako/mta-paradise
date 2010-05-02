5bfa0f5165de90e80f95ff4fa7423c5098eac2b3
mabako <mabako@gmail.com>
2010-05-02 04:42:07 +0200

    d'oh

--- resources/chat/chat.lua
+++ resources/chat/chat.lua
@@ -55,7 +55,7 @@ end
 
 -- exported function to send fake-me's
 function me( source, message )
-	localMessage( source, " *" .. getPlayerName( source ) .. ( message.sub( 1, 2 ) == "'s" and "" or " " ) .. message, 255, 40, 80 )
+	localMessage( source, " *" .. getPlayerName( source ) .. ( message:sub( 1, 2 ) == "'s" and "" or " " ) .. message, 255, 40, 80 )
 end
 
 -- overwrite MTA's default chat events
