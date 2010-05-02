249bf11971bb918bed1095169df535d83a40a650
mabako <mabako@gmail.com>
2010-05-02 06:32:59 +0200

    show version ingame

--- resources/server/showversion.lua
+++ resources/server/showversion.lua
@@ -1,4 +1,4 @@
-<!--
+--[[
 Copyright (c) 2010 MTA: Paradise
 
 This program is free software; you can redistribute it and/or modify
@@ -13,16 +13,14 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
--->
-<meta>
-	<script src="version.lua"/>
-	<script src="version.lua" type="client"/>
-	<export function="getVersion"/>
-	<export function="getVersion" type="client"/>
-	<script src="showversion.lua" type="client"/>
-	
-	<script src="credits_c.lua" type="client"/>
-	<script src="startup.lua"/>
-	
-	<script src="1.0.4.lua"/>
-</meta>
+]]
+
+addEventHandler( "onClientResourceStart", resourceRoot,
+	function( )
+		local screenX, screenY = guiGetScreenSize( )
+		local label = guiCreateLabel( 0, 0, screenX, 15, "MTA: Paradise " .. getVersion( ), false )
+		guiSetSize( label, guiLabelGetTextExtent( label ) + 5, 14, false )
+		guiSetPosition( label, screenX - guiLabelGetTextExtent( label ) - 5, screenY - 27, false )
+		guiSetAlpha( label, 0.5 )
+	end
+)
