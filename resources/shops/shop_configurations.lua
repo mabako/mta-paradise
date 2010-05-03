f3f722c2da2e18bf4d8ffc3ce91e2931013eb57b
mabako <mabako@gmail.com>
2010-05-03 10:34:19 +0200

    added well stacked pizza meals

--- resources/shops/shop_configurations.lua
+++ resources/shops/shop_configurations.lua
@@ -22,7 +22,7 @@ shop_configurations =
 		skin = 167,
 		{ itemID = 3, itemValue = 10, name = "Cluckin' Little Meal", description = "Contains french fries, a Little Clucker Kids Meal and a can of Sprunk.", price = 2 },
 		{ itemID = 3, itemValue = 30, name = "Cluckin' Big Meal", description = "Contains french fries, a Fillet Burger, a Fowl Wrap and a can of Sprunk.", price = 5 },
-		{ itemID = 3, itemValue = 50, name = "Cluckin' Huge Meal", description = "Contains french fries, a Double Fillet Burger, a Wing Pieces and a can of Sprunk.", price = 10 },
+		{ itemID = 3, itemValue = 55, name = "Cluckin' Huge Meal", description = "Contains french fries, a Double Fillet Burger, a Wing Pieces and a can of Sprunk.", price = 10 },
 		{ itemID = 3, itemValue = 40, name = "Salad Meal", description = "Contains a plate of salad, Fowl Wraps, a Dip and a can of Sprunk.", price = 10 },
 		{ itemID = 4, itemValue = 30, name = "Sprunk", description = "A can of this delicious Sprunk.", price = 5 },
 		{ itemID = 4, itemValue = 20, name = "Water", description = "A bottle of crystal clear mountain water.", price = 3 },
@@ -30,10 +30,20 @@ shop_configurations =
 	burgershot =
 	{
 		skin = 205,
-		{ itemID = 3, itemValue = 10, name = "Moo Kids Meal", description = "A Little Moo's Kids Meal with a can of Sprunk.", price = 2 },
+		{ itemID = 3, itemValue = 15, name = "Moo Kids Meal", description = "A Little Moo's Kids Meal with a can of Sprunk.", price = 2 },
 		{ itemID = 3, itemValue = 30, name = "Beef Tower Meal", description = "Contains french fries, a Beef Tower burger, a Fowl Wrap and a can of Sprunk.", price = 5 },
 		{ itemID = 3, itemValue = 50, name = "Meat Stack Meal", description = "Contains french fries, a Measure the Meat-Burger and a can of Sprunk.", price = 10 },
 		{ itemID = 3, itemValue = 40, name = "Salad Meal", description = "Contains a plate of salad and a can of Sprunk.", price = 10 },
+		{ itemID = 4, itemValue = 30, name = "Sprunk", description = "A can of this delicious Sprunk.", price = 5 },
+		{ itemID = 4, itemValue = 20, name = "Water", description = "A bottle of crystal clear mountain water.", price = 3 },
+	},
+	pizza =
+	{
+		skin = 155,
+		{ itemID = 3, itemValue = 10, name = "Buster", description = "Contains french fries, a slice of Pizza and a can of Sprunk.", price = 3 },
+		{ itemID = 3, itemValue = 30, name = "Double D-Luxe", description = "Contains french fries, a slice of Pizza, a salad with chicken breast and a can of Sprunk.", price = 5 },
+		{ itemID = 3, itemValue = 50, name = "Full Rack", description = "Contains french fries, a Pizza and a can of Sprunk.", price = 10 },
+		{ itemID = 3, itemValue = 70, name = "Large Salad Meal", description = "Contains a large plate of salad and a can of Sprunk.", price = 10 },
 		{ itemID = 4, itemValue = 30, name = "Sprunk", description = "A can of this delicious Sprunk.", price = 5 },
 		{ itemID = 4, itemValue = 20, name = "Water", description = "A bottle of crystal clear mountain water.", price = 3 },
 	},
