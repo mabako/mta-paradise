d8d9f2f3b90ba6ca6f3dac1341b0524825b346f0
mabako <mabako@gmail.com>
2010-05-15 05:04:48 +0200

    irc: use the sockets module instead of VRocker's plugin - http://code.google.com/p/multitheftauto-modules/

--- resources/server/irc.lua
+++ resources/server/irc.lua
@@ -16,12 +16,95 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 ]]
 
 -- tricky, tricky. If the module is not loaded, ignore everything
-if ircInit then
+if sockOpen then
 	-- now let's fetch all irc config we have to take care of
 	local config_bots = get( 'irc-bots' )
 	
 	-- if no irc configuration is available, screw it
 	if config_bots then
+		local bots = { }
+		
+		local function ircRaw( bot, message )
+			assert( type( bot ) == "table" and bot.socket ) -- bad, bad, didn't pass a valid bot.
+			sockWrite( bot.socket, message .. "\r\n" )
+		end
+		
+		local function ircConnect( server, port, nickname, connectedCommands )
+			assert( server )
+			assert( port )
+			assert( nickname )
+			
+			if type( connectedCommands ) ~= "table" then
+				connectedCommands = { }
+			end
+			
+			table.insert( connectedCommands, 1, "USER " .. nickname .. " * * :MTA: Paradise " .. getVersion( ) )
+			table.insert( connectedCommands, 2, "NICK " .. nickname ) -- handle nick in use errors. some time.
+			
+			-- save the bot
+			table.insert( bots, { socket = sockOpen( server, port ), connected = false, queue = connectedCommands } )
+		end
+		
+		local function ircDisconnect( bot )
+			assert( type( bot ) == "table" and bot.socket ) -- bad, bad, still didn't pass a bot
+			if bot.connected then
+				ircRaw( bot, "QUIT :MTA Paradise " .. getVersion( ) )
+			end
+			sockClose( bot.socket )
+			bot.socket = nil
+			bot.connected = nil
+		end
+		
+		local function ircMessage( bot, channel, message )
+			-- if no bot is given, chose a random time
+			if not bot then
+				local connected = { }
+				for key, value in pairs( bots ) do
+					if value.connected then
+						table.insert( connected, value )
+					end
+				end
+				
+				if #connected == 0 then
+					return
+				else
+					bot = connected[ math.random( 1, #connected ) ]
+				end
+			else
+				assert( type( bot ) == "table" and bot.socket )
+			end
+			
+			ircRaw( bot, "PRIVMSG " .. channel .. " :" .. message )
+		end
+		
+		addEventHandler( "onSockOpened", resourceRoot,
+			function( socket )
+				for key, value in pairs( bots ) do
+					if value.socket == socket then
+						value.connected = true
+						
+						-- spam dozens
+						for _, command in ipairs( value.queue ) do
+							ircRaw( value, command )
+						end
+						break
+					end
+				end
+			end
+		)
+		
+		-- do this cause we want to stay connected
+		addEventHandler( "onSockData", resourceRoot,
+			function( socket, chunk )
+				-- if interested parse more data.
+				if chunk:sub( 1, 4 ) == "PING" then
+					sockWrite( socket, "PONG" .. chunk:sub( 5 ) .. "\r\n" )
+				end
+			end
+		)
+		
+		--
+		
 		local config_channels = get( 'irc-channels' ) -- first channel is our echo channel, rest is meh.
 		local config_server = get( 'irc-server' )
 		local config_port = tonumber( get( 'irc-port' ) or 6667 )
@@ -33,28 +116,20 @@ if ircInit then
 		assert( config_server and type( config_server ) == "string" and #config_server > 0 )
 		assert( config_port and config_port >= 1024 and config_port <= 65535 )
 		
-		-- initalize IRC
-		local bots = { }
-		
 		-- initalize stuff needed, connect bots, stuff
 		addEventHandler( "onResourceStart", resourceRoot,
 			function( )
-				ircInit( )
-				
 				for key, value in ipairs( config_bots ) do
-					bots[ key ] = ircOpen( config_server, config_port, value, "" )
+					local connectedCommands = { }
 					if config_nickserv_password then
-						setTimer(
-							function( )
-								ircRaw( bots[ key ], "NS IDENTIFY " .. config_nickserv_password )
-								for _, value in ipairs( config_channels ) do
-									ircJoin( bots[ key ], value )
-								end
-							end,
-							500,
-							1
-						)
+						table.insert( connectedCommands, "NS IDENTIFY " .. config_nickserv_password )
 					end
+					
+					for key, value in ipairs( config_channels ) do
+						table.insert( connectedCommands, "JOIN " .. value )
+					end
+					
+					ircConnect( config_server, config_port, value, connectedCommands )
 				end
 			end
 		)
@@ -69,7 +144,7 @@ if ircInit then
 		
 		-- send a message to irc
 		function message( message )
-			ircMessage( bots[ math.random( 1, #bots ) ], config_channels[1], message:gsub( "%%C", "\003" ):gsub( "%%B", "\002" ) )
+			ircMessage( nil, config_channels[1], message:gsub( "%%C", "\003" ):gsub( "%%B", "\002" ) )
 		end
 		
 		-- add some default handlers (a little delayed for anti-spam reasons when starting)
