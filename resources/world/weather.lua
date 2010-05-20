333007ab0fa5e78fc445462a7ad28d14363f501e
mabako <mabako@gmail.com>
2010-05-20 20:24:41 +0200

    weather: use San Francisco weather

--- resources/world/weather.lua
+++ resources/world/weather.lua
@@ -15,33 +15,52 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 ]]
 
--- weather table that tells us how often which weather does occur
-local weather =
-{
-	-- not all weathers make sense/look good at all times, this should prolly be fixed (alternative suggestion: make a table and make the weather occurences managable via admin/clicks (never - seldom - average - more than usual - often)
-	sunny = { 0, 1, 10, 11, 17, 18, chance = 6 },
-	clouds = { 2, 3, 4, 5, 6, 7, chance = 10 },
-	fog = { 9, chance = 3 },
-	rainy = { 8, 16, chance = 2 },
-	dull = { 12, 13, 14, 15, chance = 8 },
-}
-
--- transform it into a table with chances n stuff
-local weather_ = { }
-for key, value in pairs( weather ) do
-	value.name = key
-	for i = 1, ( value.chance or 5 ) do
-		table.insert( weather_, value )
-	end
-end
+local remoteAddress = get( 'weather' )
 
 --
 
+local weather =
+{
+	-- not all weathers make sense/look good at all times, this should prolly be fixed (alternative suggestion: make a table and make the weather occurences managable via admin/clicks (never - seldom - average - more than usual - often)
+	sunny = { 0, 1, 10, 11, 17, 18 },
+	clouds = { 2, 3, 4, 5, 6, 7 },
+	fog = { 9 },
+	stormy = { 8 },
+	rainy = { 16 },
+	dull = { 12, 13, 14, 15 },
+}
+
+--
+
+local function setWeatherEx( str )
+	setWeather( weather[str][ math.random( #weather[str] ) ] )
+end
+
+local function setWeatherFromRemote( w, e )
+	if w == "ERROR" then
+		outputDebugString( "Weather: " .. remoteAddress .. " returned an error: " .. e, 2 )
+	elseif w == nil then
+		outputDebugString( "Weather: " .. remoteAddress .. " returned no usable data.", 2 )
+	else
+		if w == 'sunny' or w == 'mostly sunny' or w == 'chance of storm' then
+			setWeatherEx( 'sunny' )
+		elseif w == 'partly cloudy' or w == 'mostly cloudy' or w == 'smoke' or w == 'cloudy' then
+			setWeatherEx( 'clouds' )
+		elseif w == 'showers' or w == 'rain' or w == 'chance of rain' then
+			setWeatherEx( 'rainy' )
+		elseif w == 'storm' or w == 'thunderstorm' or w == 'chance of tstorm' then
+			setWeatherEx( 'stormy' )
+		elseif w == 'fog' or w == 'icy' or w == 'snow' or w == 'chance of snow' or w == 'flurries' or w == 'sleet' or w == 'mist' then
+			setWeatherEx( 'fog' )
+		elseif w == 'dust' or w == 'haze' then
+			setWeatherEx( 'dull' )
+		end
+	end
+end
+
 local function updateWeather( )
 	-- find a new weather
-	local weather = weather_[ math.random( 1, #weather_ ) ]
-	weather = weather[ math.random( 1, #weather ) ]
-	setWeather( weather )
+	callRemote( remoteAddress, setWeatherFromRemote )
 end
 
 addEventHandler( "onResourceStart", resourceRoot,
