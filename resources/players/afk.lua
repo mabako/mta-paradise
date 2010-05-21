565fa6e9819b251beecf5048f73d9cd4fc893e8e
mabako <mabako@gmail.com>
2010-05-21 20:59:07 +0200

    players: AFK-Kick

---
+++ resources/players/afk.lua
@@ -0,0 +1,34 @@
+--[[
+Copyright (c) 2010 MTA: Paradise
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+]]
+
+local afkTime = tonumber( get( 'afk_time' ) ) or 300 -- Time you can be away (in seconds) before you'll be kicked from the server.
+
+addEventHandler( "onResourceStart", resourceRoot,
+	function( )
+		setElementData( resourceRoot, "afk_time", afkTime )
+	end
+)
+
+addEvent( getResourceName( resource ) .. ":afk", true )
+addEventHandler( getResourceName( resource ) .. ":afk", root,
+	function( )
+		if source == client then
+			kickPlayer( source, "Away from Keyboard" )
+		end
+	end
+)
+
---
+++ resources/players/afk_c.lua
@@ -0,0 +1,47 @@
+--[[
+Copyright (c) 2010 MTA: Paradise
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+]]
+
+local afkTime = 0
+local timer = setTimer(
+	function( )
+		-- this runs once every 10 seconds, thus might not be completely accurate
+		afkTime = afkTime + 10
+		if afkTime > getElementData( resourceRoot, "afk_time" ) then
+			triggerServerEvent( getResourceName( resource ) .. ":afk", getLocalPlayer( ) )
+		end
+	end,
+	10000,
+	0
+)
+
+local function reset( )
+	afkTime = 0
+end
+
+addEventHandler( "onClientResourceStart", resourceRoot,
+	function( )
+		-- this is mostly triggered by moving the cursor and/or using the chatbox
+		addEventHandler( "onClientCursorMove", root, reset )
+		
+		-- if the control state changes, the player is most likely there...
+		local controls = { 'fire', 'next_weapon', 'previous_weapon', 'forwards', 'backwards', 'left', 'right', 'zoom_in', 'zoom_out', 'change_camera', 'jump', 'sprint', 'look_behind', 'crouch', 'walk', 'aim_weapon', 'enter_exit', 'vehicle_fire', 'vehicle_secondary_fire', 'vehicle_left', 'vehicle_right', 'steer_forward', 'steer_back', 'accelerate', 'brake_reverse', 'horn', 'sub_mission', 'vehicle_look_left', 'vehicle_look_right', 'vehicle_look_behind', 'vehicle_mouse_look', 'special_control_left', 'special_control_right', 'special_control_up', 'special_control_down' }
+		for key, value in ipairs( controls ) do
+			bindKey( value, "both", reset )
+		end
+	end
+)
+
