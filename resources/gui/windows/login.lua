ee7f9f1e2cd809f85c36036ab55423c8a28619d8
mabako <mabako@gmail.com>
2010-08-17 03:53:56 +0200

    experimental registration fix

--- resources/gui/windows/login.lua
+++ resources/gui/windows/login.lua
@@ -15,162 +15,164 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 ]]
 
-setTimer(
-	function( )
-		local messageTimer
-		local messageCount = 0
-		local function setMessage( text )
-			windows.login[#windows.login].text = text
-			if messageTimer then
-				killTimer( messageTimer )
+local messageTimer
+local messageCount = 0
+local function setMessage( text )
+	windows.login[#windows.login].text = text
+	if messageTimer then
+		killTimer( messageTimer )
+	end
+	messageCount = 0
+	setTimer(
+		function()
+			messageCount = messageCount + 1
+			if messageCount == 50 then
+				windows.login[#windows.login].text = ""
+				messageTimer = nil
+			else
+				windows.login[#windows.login].color = { 255, 255, 255, 5 * ( 50 - messageCount ) }
 			end
-			messageCount = 0
-			setTimer(
-				function()
-					messageCount = messageCount + 1
-					if messageCount == 50 then
-						windows.login[#windows.login].text = ""
-						messageTimer = nil
-					else
-						windows.login[#windows.login].color = { 255, 255, 255, 5 * ( 50 - messageCount ) }
-					end
-				end, 100, 50
-			)
+		end, 100, 50
+	)
+end
+
+local function tryLogin( key )
+	if key ~= 2 and destroy and destroy['g:login:username'] and destroy['g:login:password'] then
+		local u = guiGetText( destroy['g:login:username'] )
+		local p = guiGetText( destroy['g:login:password'] )
+		if u and p then
+			if #u == 0 then
+				setMessage( "Please enter a username." )
+			elseif #p == 0 then
+				setMessage( "Please enter a password." )
+			else
+				triggerServerEvent( "players:login", getLocalPlayer( ), u, p )
+			end
 		end
-		
-		local function tryLogin( key )
-			if key ~= 2 and destroy and destroy['g:login:username'] and destroy['g:login:password'] then
-				local u = guiGetText( destroy['g:login:username'] )
-				local p = guiGetText( destroy['g:login:password'] )
-				if u and p then
-					if #u == 0 then
-						setMessage( "Please enter a username." )
-					elseif #p == 0 then
-						setMessage( "Please enter a password." )
-					else
-						triggerServerEvent( "players:login", getLocalPlayer( ), u, p )
-					end
+	end
+end
+
+windows.login =
+{
+	snapTop = true,
+	snapBottom = true,
+	onCreate = function( )
+			for i = #windows.login, 1, -1 do
+				if windows.login[i].text == "Register" then
+					table.remove( windows.login, i )
 				end
 			end
-		end
-		
-		windows.login =
-		{
-			snapTop = true,
-			snapBottom = true,
-			{
-				type = "label",
-				text = "Welcome!",
-				font = "bankgothic",
-				alignX = "center",
-			},
-			{
-				type = "label",
-				text = "You need an account to play on this server.\nIf you already have an account, please login.",
-				alignX = "center",
-			},
-			{
-				type = "label",
-				text = getElementData( getResourceRootElement( getResourceFromName( "players" ) ), "allowRegistration" ) and "Alternatively, you may register below." or getElementData( getResourceRootElement( getResourceFromName( "players" ) ), "registrationErrorMessage" ) or "Registration is currently disabled.",
-				alignX = "center",
-			},
-			{
-				type = "edit",
-				text = "Username:",
-				id = "g:login:username",
-				onAccepted = tryLogin,
-			},
-			{
-				type = "edit",
-				text = "Password:",
-				id = "g:login:password",
-				masked = true,
-				onAccepted = tryLogin,
-			},
-			{
-				type = "button",
-				text = "Login",
-				onClick = tryLogin,
-			}
-		}
-		
-		if getElementData( getResourceRootElement( getResourceFromName( "players" ) ), "allowRegistration" ) then
-			table.insert( windows.login,
-				{
-					type = "button",
-					text = "Register",
-					onClick = function( key )
-							if key == 1 and destroy and destroy['g:login:username'] and destroy['g:login:password'] then
-								local u = guiGetText( destroy['g:login:username'] )
-								local p = guiGetText( destroy['g:login:password'] )
-								if u and p then
-									if #u < 3 then
-										setMessage( "Your username needs to be at least 3 chars." )
-									elseif #p < 8 then
-										setMessage( "Your password needs to be at least 8 chars." )
-									else
-										triggerServerEvent( "players:register", getLocalPlayer( ), u, p )
+			
+			if getElementData( getResourceRootElement( getResourceFromName( "players" ) ), "allowRegistration" ) then
+				table.insert( windows.login, #windows.login,
+					{
+						type = "button",
+						text = "Register",
+						onClick = function( key )
+								if key == 1 and destroy and destroy['g:login:username'] and destroy['g:login:password'] then
+									local u = guiGetText( destroy['g:login:username'] )
+									local p = guiGetText( destroy['g:login:password'] )
+									if u and p then
+										if #u < 3 then
+											setMessage( "Your username needs to be at least 3 chars." )
+										elseif #p < 8 then
+											setMessage( "Your password needs to be at least 8 chars." )
+										else
+											triggerServerEvent( "players:register", getLocalPlayer( ), u, p )
+										end
 									end
 								end
 							end
-						end
-				}
-			)
+					}
+				)
+			end
+		end,
+	{
+		type = "label",
+		text = "Welcome!",
+		font = "bankgothic",
+		alignX = "center",
+	},
+	{
+		type = "label",
+		text = "You need an account to play on this server.\nIf you already have an account, please login.",
+		alignX = "center",
+	},
+	{
+		type = "label",
+		text = function( ) return getElementData( getResourceRootElement( getResourceFromName( "players" ) ), "allowRegistration" ) and "Alternatively, you may register below." or getElementData( getResourceRootElement( getResourceFromName( "players" ) ), "registrationErrorMessage" ) or "Registration is currently disabled." end,
+		alignX = "center",
+	},
+	{
+		type = "edit",
+		text = "Username:",
+		id = "g:login:username",
+		onAccepted = tryLogin,
+	},
+	{
+		type = "edit",
+		text = "Password:",
+		id = "g:login:password",
+		masked = true,
+		onAccepted = tryLogin,
+	},
+	{
+		type = "button",
+		text = "Login",
+		onClick = tryLogin,
+	}
+}
+
+
+table.insert( windows.login, { type = "label", text = "", alignX = "center" } )
+
+addEvent( "players:loginResult", true )
+addEventHandler( "players:loginResult", getLocalPlayer( ),
+	function( code )
+		if code == 1 then
+			setMessage( "Wrong username or password." )
+		elseif code == 2 then
+			show( 'banned', true )
+		elseif code == 3 then
+			show( 'activation_required', true )
+		elseif code == 4 then
+			setMessage( "Unknown Error." )
+		elseif code == 5 then
+			setMessage( "Another user is currently\nlogged in under that account." )
 		end
-		
-		table.insert( windows.login, { type = "label", text = "", alignX = "center" } )
-		
-		addEvent( "players:loginResult", true )
-		addEventHandler( "players:loginResult", getLocalPlayer( ),
-			function( code )
-				if code == 1 then
-					setMessage( "Wrong username or password." )
-				elseif code == 2 then
-					show( 'banned', true )
-				elseif code == 3 then
-					show( 'activation_required', true )
-				elseif code == 4 then
-					setMessage( "Unknown Error." )
-				elseif code == 5 then
-					setMessage( "Another user is currently\nlogged in under that account." )
-				end
-			end
-		)
-		
-		addEvent( "players:registrationResult", true )
-		addEventHandler( "players:registrationResult", getLocalPlayer( ),
-			function( code, message )
-				if code == 0 then
-					tryLogin( )
-				elseif code == 1 then
-					setMessage( "Try later." )
-				elseif code == 2 then
-					setMessage( "Registration is disabled." )
-				elseif code == 3 then
-					setMessage( "This username already exists." )
-				elseif code == 4 then
-					setMessage( "Database error." )
-				end
-			end
-		)
-		
-		windows.banned =
-		{
-			type = "label",
-			text = "You are banned.",
-			font = "bankgothic",
-			color = { 255, 0, 0, 255 },
-			alignX = "center",
-		}
-		
-		windows.activation_required =
-		{
-			type = "label",
-			text = "Please activate\nyour account.",
-			font = "bankgothic",
-			alignX = "center",
-		}
-	end,
-	100,
-	1
+	end
 )
+
+addEvent( "players:registrationResult", true )
+addEventHandler( "players:registrationResult", getLocalPlayer( ),
+	function( code, message )
+		if code == 0 then
+			tryLogin( )
+		elseif code == 1 then
+			setMessage( "Try later." )
+		elseif code == 2 then
+			setMessage( "Registration is disabled." )
+		elseif code == 3 then
+			setMessage( "This username already exists." )
+		elseif code == 4 then
+			setMessage( "Database error." )
+		end
+	end
+)
+
+windows.banned =
+{
+	type = "label",
+	text = "You are banned.",
+	font = "bankgothic",
+	color = { 255, 0, 0, 255 },
+	alignX = "center",
+}
+
+windows.activation_required =
+{
+	type = "label",
+	text = "Please activate\nyour account.",
+	font = "bankgothic",
+	alignX = "center",
+}
