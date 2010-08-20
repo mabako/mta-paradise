55d27efc56a30d9d4afb9366f512f3e81d5fe4e3
mabako <mabako@gmail.com>
2010-08-21 00:26:57 +0200

    players: respawn always works on pressing enter

--- resources/players/respawn_c.lua
+++ resources/players/respawn_c.lua
@@ -15,56 +15,40 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 ]]
 
-local respawnKeys = { 'enter_exit', 'fire' }
+local respawnKeys = { 'enter' }
 
 --
 
-local respawnKey = false
 local respawnWait = false
 local localPlayer = getLocalPlayer( )
 local screenX, screenY = guiGetScreenSize( )
 
 function drawRespawnText( )
-	-- makes only sense if we actually found a key to respawn - if not: player can't enter vehicles (makes no sense)
-	if respawnKey then
-		local text = "Press '" .. respawnKey .. "' to respawn"
-		
-		-- check if we still need to wait
-		if respawnWait then
-			local diff = respawnWait - getTickCount( )
-			if diff >= 0 then
-				text = ( "Wait %.1f seconds to respawn" ):format( diff / 1000 )
-			elseif isCursorShowing( ) then
-				-- check if the player presses a control, wouldn't be caught by SA as the key is down
-				for key, value in ipairs( respawnKeys ) do
-					local keys = getBoundKeys( value )
-					if keys then
-						for k in pairs( keys ) do
-							if not k:find( 'mouse' ) and getKeyState( k ) then
-								requestRespawn( )
-								break
-							end
-						end
-					end
+	local text = "Press '" .. respawnKeys[1] .. "' to respawn"
+	
+	-- check if we still need to wait
+	if respawnWait then
+		local diff = respawnWait - getTickCount( )
+		if diff >= 0 then
+			text = ( "Wait %.1f seconds to respawn" ):format( diff / 1000 )
+		else
+			-- check if the player presses a control, wouldn't be caught by SA as the key is down
+			for key, value in ipairs( respawnKeys ) do
+				if getKeyState( value ) then
+					requestRespawn( )
+					break
 				end
 			end
 		end
-		
-		-- draw the text
-		dxDrawText( text, 4, 4, screenX, screenY, tocolor( 0, 0, 0, 255 ), 1, "pricedown", "center", "center" )
-		dxDrawText( text, 0, 0, screenX, screenY, tocolor( 255, 255, 255, 255 ), 1, "pricedown", "center", "center" )
 	end
+	
+	-- draw the text
+	dxDrawText( text, 4, 4, screenX, screenY, tocolor( 0, 0, 0, 255 ), 1, "pricedown", "center", "center" )
+	dxDrawText( text, 0, 0, screenX, screenY, tocolor( 255, 255, 255, 255 ), 1, "pricedown", "center", "center" )
 end
 
 function requestRespawn( )
 	if isPlayerDead( localPlayer ) and respawnWait and respawnWait - getTickCount( ) < 0 then
-		-- restore all keys
-		for key, value in ipairs( respawnKeys ) do
-			bindKey( value, 'down', requestRespawn )
-			if value == 'fire' then
-				toggleControl( value, true )
-			end
-		end
 		respawnWait = false
 		removeEventHandler( "onClientRender", root, drawRespawnText )
 		
@@ -76,32 +60,11 @@ end
 addEventHandler( "onClientPlayerWasted", localPlayer,
 	function( )
 		-- keep the camera (reset when the player respawns)
-		setCameraMatrix( getCameraMatrix( ) )
+		local a, b, c = getCameraMatrix( )
+		local d, e, f = getElementPosition( localPlayer )
+		setCameraMatrix( a, b, c, d, e, f )
 		
-		-- bind all configured keys usable for respawn
-		for key, value in ipairs( respawnKeys ) do
-			bindKey( value, 'down', requestRespawn )
-			if value == 'fire' then
-				toggleControl( value, false )
-			end
-		end
-		
-		-- find a key that can be used for respawning, display it later
-		respawnKey = false
 		respawnWait = getTickCount( ) + getElementData( resourceRoot, 'respawnDelay' ) * 1000
-		for key, value in ipairs( respawnKeys ) do
-			local keys = getBoundKeys( value )
-			if keys then
-				for k in pairs( keys ) do
-					respawnKey = k
-					break
-				end
-			end
-			
-			if respawnKey then
-				break
-			end
-		end
 		addEventHandler( "onClientRender", root, drawRespawnText )
 	end
 )
