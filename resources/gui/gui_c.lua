c2e0919d43669d68f447d000735b954a2e8f605c
mabako <mabako@gmail.com>
2010-08-31 18:07:33 +0200

    gui: languages gui doesn't open when pressing 'L' when in a vehicle (as it's for lights)

--- resources/gui/gui_c.lua
+++ resources/gui/gui_c.lua
@@ -491,7 +491,7 @@ bindKey( 'i', 'down',
 
 bindKey( 'l', 'down',
 	function( )
-		if not forcedWindow then
+		if not forcedWindow and not isPedInVehicle( getLocalPlayer( ) ) then
 			if window == windows.languages then
 				hide( )
 			else
